bdac389845e8e9b8bdf45eb90a4e6c1742411910
tab1k <tab1k@example.com>
2026-05-12 13:27:24 +0500

    feat: добавление поддержки обработки медиафайлов в настройках и маршрутах

--- src/dokerler/settings.py
+++ src/dokerler/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import sys
 from pathlib import Path
 import os
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # Local apps
     'base.apps.BaseConfig',
     'products',
 ]
--- src/dokerler/urls.py
+++ src/dokerler/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.i18n import i18n_patterns
+from django.views.static import serve
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -18,3 +19,6 @@ urlpatterns += i18n_patterns(
 # Serve uploaded media files. In this project deployment there is no separate
 # web server location for /media, so Django must expose MEDIA_URL routes.
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
